0d777844fb6083be23a659746eeae33eccf783cd
Jaroslav Hron <hron@karlin.mff.cuni.cz>
2014-06-25 12:25:14 +0200

    Clean up and update acording to mathjax.org

--- lib/redmine_latex_mathjax/hooks/view_layouts_base_html_head_hook.rb
+++ lib/redmine_latex_mathjax/hooks/view_layouts_base_html_head_hook.rb
@@ -2,8 +2,9 @@ module RedmineLatexMathjax
   module Hooks
     class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-          return "<script type=\"text/x-mathjax-config\">
-          MathJax.Hub.Config({
+        return "
+    <script type=\"text/javascript\">
+    window.MathJax = {
     extensions: ['tex2jax.js'],
     jax: ['input/TeX', 'output/HTML-CSS'],
     tex2jax: {
@@ -12,10 +13,9 @@ module RedmineLatexMathjax
       processEscapes: true
     },
     'HTML-CSS': { availableFonts: ['TeX'] }
-  });
-          MathJax.Hub.Typeset();
-          </script>" +
-            javascript_include_tag('https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML')
+    }
+    </script>" 
+        + javascript_include_tag('https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML&delayStartupUntil=onload')
       end
     end
   end
